b4243223ba648960b94f51e362f0bdc66aff110d
Dana Simmons <dsanalog@gmail.com>
2019-08-05 22:29:56 -0400

    Create DTMF decoder script
    decode-dtmf.sh uses rtl_fm and multimon-ng to decode
    realtime dtmf signals. set the $FREQ variable to
    adjust the tunning frequency. DTMF codes are printed
    on the standard output

---
+++ decode-dtmf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+##############################################################
+#
+# Decode DTMF script: reads raw input from RTL-SDR receiver
+# prints DTMF charachters to STDOUT
+# 2019 Dana Simmons
+#
+##############################################################
+FREQ="146.520M"
+
+
+rtl_fm -s 22050 -f $FREQ | multimon-ng -t raw -a DTMF -
